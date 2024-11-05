bf34dda949e64ea416c7a9637fb361e14ccf2304
Gravita <12893402+gravit0@users.noreply.github.com>
2024-11-06 05:40:55 +0700

    Support multiple change prefix products

--- src/components/dialogs/ChangePrefixDialog.vue
+++ src/components/dialogs/ChangePrefixDialog.vue
@@ -11,14 +11,17 @@
           :rules="[val => val && val.length > 0 || 'Введите новый префикс']">
         </q-input>
       </q-card-section>
+      <q-card-section v-if="paided == null">
+        Смена префикса недоступна
+      </q-card-section>
       <q-card-section v-if="paided == false">
-        Стоимость смены префикса будет составлять {{ price }} {{ currency }}
+        <q-select map-options option-label="description" v-model="product" :options="products" label="Название услуги"></q-select>
+        Стоимость смены префикса будет составлять {{ product.price }} {{ product.currency }}
       </q-card-section>
       <q-card-section v-if="paided == true">
         Вам доступна смена префикса бесплатно
       </q-card-section>
       <q-separator dark></q-separator>
-
       <q-card-actions>
         <q-btn flat color="primary" @click="changePrefix">Изменить</q-btn>
       </q-card-actions>
@@ -35,11 +38,10 @@ export default defineComponent({
     const $store = useStore();
     const $q = useQuasar();
     const newPrefix = ref(null);
-    const paided = ref(false);
-    const price = ref(null);
-    const currency = ref(null);
+    const paided = ref(null);
     const orderId = ref(null);
-    const productId = ref(null);
+    const product = ref(null);
+    const products = ref([]);
     let show = ref(false);
     async function checkPrefixPrice() {
       var result = await $store.dispatch("api/request", {
@@ -54,13 +56,15 @@ export default defineComponent({
         }
       }
       var result2 = await $store.dispatch("api/request", {
-          url: "shop/service/type/CHANGE_PREFIX",
+          url: "shop/service/type/CHANGE_PREFIX/0",
           method: "GET",
       });
       if(result2.ok) {
-          productId.value = result2.data.id;
-          price.value = result2.data.price;
-          currency.value = result2.data.currency;
+          products.value = result2.data.data;
+          product.value = result2.data.data[0];
+      } else {
+        products.value = [];
+        product.value = null;
       }
       var result2 = await $store.dispatch("api/request", {
           url: "shop/service/type/CHANGE_PREFIX/order",
@@ -69,6 +73,9 @@ export default defineComponent({
       if(result2.ok) {
         orderId.value = result2.data.id;
         paided.value = true;
+      } else {
+        orderId.value = 0;
+        paided.value = false;
       }
     }
     checkPrefixPrice();
@@ -79,7 +86,7 @@ export default defineComponent({
           url: "shop/service/buy",
           method: "POST",
           body: {
-            id: productId.value,
+            id: product.value.id,
             quantity: 1
           },
         });
@@ -89,6 +96,7 @@ export default defineComponent({
             "type": "negative",
             "message": "Ошибка при смене префикса: SC" + error.code + ": " + error.error
           })
+          checkPrefixPrice();
           return;
         }
         }
@@ -104,6 +112,7 @@ export default defineComponent({
             "type": "positive",
             "message": "Префикс успешно изменен"
           })
+          checkPrefixPrice();
           show = false
         } else {
           var error = result.data
@@ -111,10 +120,11 @@ export default defineComponent({
             "type": "negative",
             "message": "Ошибка при смене префикса: SC" + error.code + ": " + error.error
           })
+          checkPrefixPrice();
         }
       },
-      price,
-      currency,
+      product,
+      products,
       paided,
       newPrefix,
       show
