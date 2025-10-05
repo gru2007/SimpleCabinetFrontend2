1d49ad28a4fa64fc69394922265b6d42cdfe5eb1
Gru <78332542+gru2007@users.noreply.github.com>
2025-10-05 22:50:28 +1000

    Update state.js

--- src/store/module-api/state.js
+++ src/store/module-api/state.js
@@ -2,7 +2,7 @@ import { ref } from "vue";
 
 export default function () {
   return {
-    url: "https://mc-api.r-artemev.ru",
+    url: "https://mc-api.r-artemev.ru/",
     hcaptcha: {
       sitekey: "b266dbb6-d8b2-4608-b076-dfaaefb68e9c"
     },
